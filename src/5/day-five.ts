1c4f34651d129940deeb8e00ae671bd4823b37b3
Atuuh <not-another-one@hotmail.com>
2018-12-11 23:16:39 +0000

    day 5 complete

--- src/5/day-five.ts
+++ src/5/day-five.ts
@@ -8,14 +8,36 @@ export class DayFive extends Advent {
         this.Input = input;
 
         let polymer = this.Input;
+        const finalPolymer = this.fullyActivatePolymer(polymer);
+
+        this.PartA = finalPolymer.length.toString();
+
+        const units = new Array<number>(26);
+        for (let i = 0; i < units.length; i++) {
+            const unit = this.getChar(i);
+            const strippedPolymer = polymer.replace(new RegExp(`[${unit}${unit.toUpperCase()}]`, 'g'), '');
+            units[i] = this.fullyActivatePolymer(strippedPolymer).length;
+        }
+
+        this.PartB = Math.min(...units).toString();
+    }
+
+    private getChar(input: number): string {
+        return String.fromCharCode(input + 97);
+    }
+
+    private fullyActivatePolymer(polymer: string): string {
         let newPolymer = polymer;
         do {
             polymer = newPolymer;
-            newPolymer = this.triggerPolymer(polymer);
-
-        } while (polymer !== newPolymer)
-
-        this.PartA = polymer.length.toString();
+            newPolymer = [...polymer].reduce((total, next) => {
+                if (total.slice(-1) === this.toggleCase(next)) {
+                    return total.slice(0, -1)
+                }
+                return total + next;
+            }, '' as string);
+        } while (polymer !== newPolymer);
+        return polymer;
     }
 
     private triggerPolymer(input: string): string {
@@ -26,7 +48,6 @@ export class DayFive extends Advent {
         }
         return input;
     }
-
 
     private toggleCase(input: string): string {
         if (input.length !== 1) { throw new Error('toggleCase input is not a single char'); }
