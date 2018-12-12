c8ba8b1a12b84585f54d0796396ae35a0e8913ca
Atuuh <not-another-one@hotmail.com>
2018-12-12 21:39:33 +0000

    day 6 part one

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { DayTwo } from './2/day-two';
 import { DayThree } from './3/day-three';
 import { DayFour } from './4/day-four';
 import { DayFive } from './5/day-five';
+import { DaySix } from './6/day-six';
 
 // console.log(new DayOne().Solve());
-console.log(new DayFive().Solve());
+console.log(new DaySix().Solve());
